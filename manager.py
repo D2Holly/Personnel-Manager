2da7f145a6c262ac1a1a73a319f1a58a4c35c81d
D2Holly <holly.anscombe@digi2al.co.uk>
2025-06-04 12:35:05 +0100

    First class is workinggit add --all yay

---
+++ manager.py
@@ -0,0 +1,10 @@
+class ShipBrigade:
+    def __init__(self, ship_name):
+        self.ship_name = ship_name
+        self.crew = []
+
+    def __repr__(self):
+        return self.ship_name
+
+print(ShipBrigade("Bob"))
+  
